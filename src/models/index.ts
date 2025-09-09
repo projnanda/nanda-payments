e306245a5adb9ccd2aacb04685c3132882ff9ba5
sree bhargavi balija <sreebhargavibalija@srees-MacBook-Air.local>
2025-09-09 07:02:02 -0700

    Revert "Merge: Keep unified system version, integrate with existing repository"
    This reverts commit 18fe5d1bd1823118309c83dd45ff99eb02762b1a, reversing
    changes made to 62251f12a473870fec2a32bb845c9284caf3d032.

--- src/models/index.ts
+++
@@ -1,7 +0,0 @@
-export { NP_CURRENCY, NP_SCALE } from "./constants.js";
-export { AgentModel, type AgentDoc } from "./Agent.js";
-export { WalletModel, type WalletDoc } from "./Wallet.js";
-export { TransactionModel, type TransactionDoc } from "./Transaction.js";
-export { EventModel, type EventDoc } from "./Event.js";
-export { IdemModel, type IdemDoc } from "./Idempotency.js";
-export { WalletLinkNonceModel, type WalletLinkNonceDoc } from "./WalletLinkNonce.js";
